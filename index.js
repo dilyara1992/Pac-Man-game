e5205b808eaae2fd9bd1d5e8ed137b151f83448d
dilyara1992 <dilyara.tulegenova@gmail.com>
2021-03-20 13:27:06 -0700

    added control to the pacman

--- index.js
+++ index.js
@@ -45,16 +45,100 @@ const layout = [
 //create board
 
 function createBoard(){
-    for (let i = 0; i < layout.length; i++){
+ 
+    for (let i = 0; i < layout.length;i++){
+
+        // Creating squares
         const square = document.createElement("div")
 
-        // Put square in grid
+        // Adding new squares to the grid
         grid.appendChild(square)
 
-        //Put square in squares array
-
+        // Pushing squares to square array
         squares.push(square)
+
+        // Adding Styling
+
+        if (layout[i] === 0){
+            squares[i].classList.add('pac-dot')
+        }
+        else if (layout[i] === 1){
+            squares[i].classList.add('wall')
+        }
+        else if (layout[i] === 2){
+            squares[i].classList.add('ghost-lair')
+        }
+        else if (layout[i] === 3){
+            squares[i].classList.add('power-pellet')
+        }
+        else if (layout[i] === 4){
+            squares[i].classList.add('empty')
+        }
     }
 }
 
 createBoard()
+
+
+// starting position of pacman
+let pacmanCurrentIndex = 490
+squares[pacmanCurrentIndex].classList.add("pac-man")
+
+
+//Moving Pacman with Keys and EventListeners
+
+function control(e){
+    squares[pacmanCurrentIndex].classList.remove('pac-man')
+    // down key : 40
+    // up key: 38
+    // left key: 37
+    // right key: 39
+
+    switch(e.keyCode){
+ 
+        case 37:
+            console.log("Pressed LEFT")
+            if ((pacmanCurrentIndex % width !== 0)
+            && (!squares[pacmanCurrentIndex-1].classList.contains('wall'))
+            && (!squares[pacmanCurrentIndex-1].classList.contains('ghost-lair'))){
+                pacmanCurrentIndex -= 1
+                if(pacmanCurrentIndex === 364){
+                    pacmanCurrentIndex = 391;
+                }
+            }
+            break
+        case 38:
+            console.log("pressed keyboard UP")
+            if (
+            (pacmanCurrentIndex - width > width)
+            &&(!squares[pacmanCurrentIndex - 28].classList.contains('wall'))
+            &&(!squares[pacmanCurrentIndex - 28].classList.contains('ghost-lair')))
+            {
+                pacmanCurrentIndex -= 28
+            }
+            break
+        case 39:
+            console.log("Preseed RIGHT")
+            if ((pacmanCurrentIndex % 28 < 28 - 1)
+            &&(!squares[pacmanCurrentIndex + 1].classList.contains('wall'))
+            &&(!squares[pacmanCurrentIndex + 1].classList.contains('ghost-lair'))){
+                pacmanCurrentIndex += 1
+                if(pacmanCurrentIndex === 391){
+                    pacmanCurrentIndex = 364;
+                }
+            }
+            break
+        case 40:
+            console.log("Pressed down")
+            if ((pacmanCurrentIndex + 28 < 28*28)
+            && (!squares[pacmanCurrentIndex + width].classList.contains('wall'))
+            && (!squares[pacmanCurrentIndex + width].classList.contains('ghost-lair')))
+            {
+                pacmanCurrentIndex += 28
+            }
+            break
+    }
+    squares[pacmanCurrentIndex].classList.add('pac-man')
+}
+
+document.addEventListener('keyup', control)
